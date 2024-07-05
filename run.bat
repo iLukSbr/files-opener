22fd852ea971ebdcbc7b594235ecb37aa035ea3e
iLukS <lucasyfm@hotmail.com>
2024-07-05 16:26:05 -0300

    exit

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 @echo off
 @chcp 65001 > nul
+
 cd src
 powershell.exe -ExecutionPolicy Bypass -File "src/start.ps1"
--- src/close.bat
+++ src/close.bat
@@ -1,2 +1,6 @@
+@echo off
+
 taskkill /F /IM java.exe
 taskkill /F /IM javaw.exe
+
+exit /B
